ef78e0b6fcd9e2f17a49feb3239bcfaeb0370b77
sohrab09 <sohrab.cse9@gmail.com>
2021-12-31 11:02:24 +0600

    comments add

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const http = require('http');
+const http = require("http");
 const expressServer = http.createServer(app);
 
+// Socket Server Connection
 
-// Socket Server Connection 
-
-const {Server} = require('socket.io');
+const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
+io.on("connection", function (socket) {
+  console.log("New user connected");
 
-io.on('connection', function(socket) {
-    console.log('New user connected');
+  // * Send data from server to client
 
-    // Send data from server to client
+  /* 
 
-    // setTimeout(function() {
-    //     socket.send("Hello from server");
-    // }, 5000);
+  // ? "send" is a pre-defined event handler
+  
+        setTimeout(function() {
+            socket.send("Hello from server");
+        }, 5000); 
+    */
 
-    setInterval(function() {
-        let time = new Date();
-        let t = time.getTime();
-        socket.send(t);
-    }, 1000);
-    
-    /* socket.on('disconnect', function() {
-        console.log('User disconnected');
-    }); */
+  /*
+        setInterval(function() {
+            let time = new Date();
+            let t = time.getTime();
+            socket.send(t);
+        }, 1000);
+    */
+
+  /* 
+        socket.on('disconnect', function() {
+            console.log('User disconnected');
+        }); 
+    */
+
+        // Creating Custom Events
+
+        setInterval(function() {
+            let time = new Date();
+            let t = time.getTime();
+            socket.emit("myTime", t);
+        }, 100);
 });
-
-
 
 // Index.html file connection
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
-
-
 
 // Server calling
 
-expressServer.listen(port, () =>{
-    console.log(`Express Server listening at http://localhost:${port}`)
+expressServer.listen(port, () => {
+  console.log(`Express Server listening at http://localhost:${port}`);
 });
